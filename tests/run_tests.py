839af1e6bcaed5cf07aca28bd48c2f18f3807c29
Matt Spraggs <matthew.spraggs@gmail.com>
2018-03-01 22:07:51 +0000

    Tests: Improved encapsulation in run_tests script

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -39,20 +39,63 @@ def parse_test(test_path):
     return output_lines, rvalue
 
 
-def run_tests(interpreter_path, test_paths):
+def print_failed_test(test_name, expected_data, actual_data):
+    """Prints a failed test result to stdout"""
 
-    success_counter = 0
+    expected_output, expected_retval = expected_data
+    actual_output, actual_retval = actual_data
+    
+    red = "\033[31m" if sys.platform != "win32" else ""
+    white = "\033[0m" if sys.platform != "win32" else ""
+    
+    print()
+    print("Test {:.<40}{} FAILED{}"
+          .format(test_name, red, white))
+    print("-- Expected output:")
+    
+    for line in expected_output:
+        print("    {}".format(line))
+        
+        print("-- Actual output:")
+        
+        for line in actual_output:
+            print("    {}".format(line))
+
+        print("-- Expected return value:")
+        print("    {}".format(expected_retval))
+        print("-- Actual return value:")
+        print("    {}".format(actual_retval))
+        print()
+
+
+def print_succeeded_test(test_name):
+    """Prints the a successful test result to stdout."""
+
+    green = "\033[32m" if sys.platform != "win32" else ""
+    white = "\033[0m" if sys.platform != "win32" else ""
+
+    print("Test {:.<40}{} PASSED{}"
+          .format(test_name, green, white))
+
+
+def run_tests(interpreter_path, test_paths):
+    """Run the specified interpreter over the provided test source files,
+    reporting errors as necessary."""
+
+    fail_counter = 0
+    test_counter = 0
 
     test_paths = sorted(test_paths, key=lambda p: os.path.basename(p))
 
     for test_path in test_paths:
 
         try:
-            expected_output, expected_rvalue = parse_test(test_path)
+            expected_output, expected_retval = parse_test(test_path)
         except (AssertionError, IndexError, ValueError):
             print("Unable to parse test file \"{}\". Skipping..."
                   .format(test_path))
             continue
+        test_counter += 1
 
         process = subprocess.Popen([interpreter_path, test_path],
                                    stdout=subprocess.PIPE,
@@ -61,37 +104,23 @@ def run_tests(interpreter_path, test_paths):
         actual_output = [line.strip()
                          for line in "".join(process.communicate()).split('\n')
                          if line.strip()]
-        actual_rvalue = process.returncode
+        actual_retval = process.returncode
 
         test_fname = os.path.basename(test_path)
         test_name = fname_regex.findall(test_fname)[0]
 
-        if (actual_rvalue == expected_rvalue and
+        if (actual_retval == expected_retval and
                 actual_output == expected_output):
-            print("Test \"{}\" passed!".format(test_name))
-            success_counter += 1
+            print_succeeded_test(test_name)
         else:
-            print()
-            print("Test \"{}\" failed!".format(test_name))
-            print("-- Expected output:")
-
-            for line in expected_output:
-                print("    {}".format(line))
-
-            print("-- Actual output:")
-
-            for line in actual_output:
-                print("    {}".format(line))
-
-            print("-- Expected return value:")
-            print("    {}".format(expected_rvalue))
-            print("-- Actual return value:")
-            print("    {}".format(actual_rvalue))
-            print()
+            print_failed_test(test_name,
+                              (expected_output, expected_retval),
+                              (actual_output, actual_retval))
+            fail_counter += 1
 
     print()
     print("Ran {} tests, of which {} failed."
-          .format(len(test_paths), len(test_paths) - success_counter))
+          .format(test_counter, fail_counter))
 
 
 if __name__ == "__main__":
