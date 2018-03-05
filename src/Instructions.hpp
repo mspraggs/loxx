357f660112ce0b33b92fced0790f8e1108f973e6
Matt Spraggs <matthew.spraggs@gmail.com>
2018-03-05 18:19:17 +0000

    Instructions: Added file to contain instruction enum

---
+++ src/Instructions.hpp
@@ -0,0 +1,34 @@
+/*
+ * This file is part of loxx.
+ *
+ * loxx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * loxx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created by Matt Spraggs on 05/03/2018.
+ */
+
+#ifndef LOXX_INSTRUCTIONS_HPP
+#define LOXX_INSTRUCTIONS_HPP
+
+#include <cstdint>
+
+
+namespace loxx
+{
+  enum class Instruction : std::uint8_t
+  {
+    Return,
+  };
+}
+
+#endif // LOXX_INSTRUCTIONS_HPP
