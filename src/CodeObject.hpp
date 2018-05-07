ae9ffe8e181c66c95e84c5936360f34a6429cf92
Matt Spraggs <matthew.spraggs@gmail.com>
2018-05-07 22:01:14 +0100

    CodeObject: Moved struct into its own header file

---
+++ src/CodeObject.hpp
@@ -0,0 +1,37 @@
+/*
+ * This file is part of loxx.
+ *
+ * loxx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * loxx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created by Matt Spraggs on 07/05/18.
+ */
+
+#ifndef LOXX_CODEOBJECT_HPP
+#define LOXX_CODEOBJECT_HPP
+
+#include <cstdint>
+#include <tuple>
+#include <vector>
+
+
+namespace loxx
+{
+  struct CodeObject
+  {
+    std::vector<std::uint8_t> bytecode;
+    std::vector<std::tuple<std::int8_t, std::uint8_t>> line_num_table;
+  };
+}
+
+#endif //LOXX_CODEOBJECT_HPP
