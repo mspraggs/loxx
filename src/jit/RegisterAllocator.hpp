7af0faf6f7057b35f9c24e3fa6473c04d00f0f2e
Matt Spraggs <matthew.spraggs@gmail.com>
2019-11-30 15:43:34 +0000

    RegisterAllocator: Fix file creation date

--- src/jit/RegisterAllocator.hpp
+++ src/jit/RegisterAllocator.hpp
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Created by Matt Spraggs on 23/10/2019.
+ * Created by Matt Spraggs on 23/11/2019.
  */
 
 #ifndef LOXX_JIT_REGISTERALLOCATOR_HPP
