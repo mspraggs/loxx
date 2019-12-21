82a03727ab914761e8337fcee25a8a024edcec4f
Matt Spraggs <matthew.spraggs@gmail.com>
2019-12-21 22:05:54 +0000

    AssemblyFunction: Add header and implementation

---
+++ src/jit/AssemblyFunction.cpp
@@ -0,0 +1,59 @@
+/*
+ * This file is part of loxx.
+ *
+ * loxx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * loxx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created by Matt Spraggs on 15/12/2019.
+ */
+
+#include "AssemblyFunction.hpp"
+#include "JITError.hpp"
+
+
+namespace loxx
+{
+  namespace jit
+  {
+    void AssemblyFunction::add_byte(const std::uint8_t byte)
+    {
+      if (locked_) {
+        throw JITError("invalid memory access");
+      }
+      assembly_.push_back(byte);
+    }
+
+
+    void AssemblyFunction::lock()
+    {
+      const auto result = mprotect(
+          assembly_.data(), assembly_.capacity(),
+          PROT_READ | PROT_EXEC);
+      if (result == -1) {
+        throw JITError("memory lock failure");
+      }
+      locked_ = true;
+    }
+
+
+    bool AssemblyFunction::operator() () const
+    {
+      if (not locked_) {
+        throw JITError("invalid memory access");
+      }
+
+      const auto func = reinterpret_cast<bool (*) ()>(assembly_.data());
+      return func();
+    }
+  }
+}
---
+++ src/jit/AssemblyFunction.hpp
@@ -0,0 +1,108 @@
+/*
+ * This file is part of loxx.
+ *
+ * loxx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * loxx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created by Matt Spraggs on 15/12/2018.
+ */
+
+#ifndef LOXX_JIT_ASSEMBLYFUNCTION_HPP
+#define LOXX_JIT_ASSEMBLYFUNCTION_HPP
+
+#include <cstdint>
+#include <cstdlib>
+#include <limits>
+#include <vector>
+
+// TODO: Windows support
+#include <sys/mman.h>
+
+
+namespace loxx
+{
+  namespace jit
+  {
+    class AssemblyFunction
+    {
+    public:
+      AssemblyFunction(const std::size_t reserve_size = 4096)
+          : locked_(false)
+      {
+        assembly_.reserve(reserve_size);
+      }
+      
+      void add_byte(const std::uint8_t byte);
+
+      template <typename Iter>
+      void add_bytes(const Iter begin, const Iter end);
+
+      void lock();
+
+      bool operator() () const;
+
+    private:
+      template <typename T>
+      struct MmapAllocator
+      {
+        using value_type = T;
+
+        T* allocate(const std::size_t n);
+        void deallocate(T* ptr, const std::size_t n);
+      };
+
+      bool locked_;
+      std::vector<std::uint8_t, MmapAllocator<std::uint8_t>> assembly_;
+    };
+
+
+    template <typename Iter>
+    void AssemblyFunction::add_bytes(const Iter begin, const Iter end)
+    {
+      for (auto it = begin; it != end; ++it) {
+        add_byte(*it);
+      }
+    }
+
+
+    template <typename T>
+    T* AssemblyFunction::MmapAllocator<T>::allocate(const std::size_t n)
+    {
+      if (n > std::numeric_limits<std::size_t>::max() / sizeof(T)) {
+        throw std::bad_alloc();
+      }
+
+      auto ptr = static_cast<T*>(mmap(
+          nullptr, n * sizeof(T),
+          PROT_READ | PROT_WRITE | PROT_EXEC,
+          MAP_PRIVATE | MAP_ANONYMOUS,
+          -1, 0));
+
+      if (ptr == nullptr) {
+        throw std::bad_alloc();
+      }
+
+      return ptr;
+    }
+
+
+    template <typename T>
+    void AssemblyFunction::MmapAllocator<T>::deallocate(
+        T* ptr, const std::size_t n)
+    {
+      munmap(ptr, n);
+    }
+  }
+}
+
+#endif // LOXX_JIT_ASSEMBLYFUNCTION_HPP
